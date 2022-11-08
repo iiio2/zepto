acd128656e62c955f379962b5ef915f146b932d6
Mr. Robot <gomexxxx71@gmail.com>
2022-11-08 15:25:02 +0600

    font uploaded and showing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import _ from "lodash";
 import { FileUploader } from "react-drag-drop-files";
 
 const fileTypes = ["TTF"];
@@ -8,17 +9,27 @@ function App() {
   const handleChange = (file) => {
     setFile(file);
   };
+
+  let total = [];
+  if (file) {
+    console.log(file);
+    for (let i = 0; i < file.length; i++) {
+      const names = _.pick(file[i], ["name"]);
+      let getNames = names.name.replace(".ttf", "");
+
+      total.push(getNames);
+    }
+  }
+  console.log(total);
+  console.log(total);
   return (
     <main className="container mt-3">
-      <h3>Hello World</h3>
       <FileUploader
         multiple={true}
         handleChange={handleChange}
         name="file"
         types={fileTypes}
       />
-
-      <p>{file ? `File name: ${file[0].name}` : "no files uploaded yet"}</p>
 
       <div className="show-fonts">
         <h4>our fonts</h4>
@@ -31,6 +42,15 @@ function App() {
               <th></th>
             </tr>
           </thead>
+          <tbody>
+            {total.map((font, index) => (
+              <tr key={index}>
+                <td>{font}</td>
+                <td>Example Style</td>
+                <td className="text-danger">Delete</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </div>
     </main>
